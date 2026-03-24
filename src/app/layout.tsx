d30d894b46d5638b5d6f68bb6a7cb0cfb345b566
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2026-03-23 21:30:07 -0300

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Geist_Mono, JetBrains_Mono, Inter } from 'next/font/google';
 import './globals.css';
+import { cn } from '@/lib/utils';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  variable: '--font-mono',
+});
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,7 +33,16 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn(
+        'h-full',
+        'antialiased',
+        geistSans.variable,
+        geistMono.variable,
+        'font-mono',
+        jetbrainsMono.variable,
+        'font-sans',
+        inter.variable
+      )}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
